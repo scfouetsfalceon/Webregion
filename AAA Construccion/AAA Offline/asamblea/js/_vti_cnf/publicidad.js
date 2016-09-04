4e7990795892c11771ba2c091e7f4d2c38c9f5b1
Caballero73 <comisionado.falcon@gmail.com>
2016-09-03 21:07:28 -0400

    Directorio eliminado

--- AAA Construccion/AAA Offline/asamblea/js/_vti_cnf/publicidad.js
+++
@@ -1,4 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|28 Jan 2011 05:07:08 -0000
-vti_extenderversion:SR|6.0.2.5516
-vti_backlinkinfo:VX|inscripcion_manual.html index.html inscripcion.html
